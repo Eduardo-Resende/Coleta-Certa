51268755a9a8ebd69bdc9b2c81a649c62fde315a
Eduardo Santana <santana.edu2004@gmail.com>
2025-03-30 01:04:20 -0300

    Criando tela inicial (loading)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:coleta_certa/ui/loading_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class ColetaCertaApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Text("Coleta Certa")
+      home: LoadingScreen()
     );
   }
 }
