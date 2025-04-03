1a463682467815d6018fd0ad9539f1faf982c96f
Eduardo Santana <santana.edu2004@gmail.com>
2025-04-03 12:51:48 -0300

    Adicionando imagem para visualização do prototipo

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -31,56 +31,72 @@ class MainScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        color: Colors.grey[600],
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(const Color.fromARGB(255, 219, 216, 216)),
-                  elevation: WidgetStatePropertyAll(10),
-                ),
-                onPressed: () {},
-                child: Icon(
-                  Icons.access_time_filled_sharp,
-                  color: Colors.green,
-                  size: 50,
-                ),
-              ),
-              
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(const Color.fromARGB(255, 219, 216, 216)),
-                  elevation: WidgetStatePropertyAll(10),
-                ),
-                onPressed: () {},
-                child: Icon(
-                  Icons.location_on,
-                  color: Colors.green,
-                  size: 50,
-                ),
-              ),
-              
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(const Color.fromARGB(255, 219, 216, 216)),
-                  elevation: WidgetStatePropertyAll(10),
-                ),
-                onPressed: () {},
-                child: Icon(
-                  Icons.restore_from_trash_rounded,
-                  color: Colors.green,
-                  size: 50,
-                ),
-              ),
-            ],
+      body: Stack(
+        children: [
+          Image.asset(
+            'lib/assets/img/mapa.jpg',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
           ),
-        ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            color: Color.fromARGB(0, 75, 73, 73),
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(
+                        const Color.fromARGB(255, 219, 216, 216),
+                      ),
+                      elevation: WidgetStatePropertyAll(10),
+                    ),
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.access_time_filled_sharp,
+                      color: Colors.green,
+                      size: 50,
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(
+                        const Color.fromARGB(255, 219, 216, 216),
+                      ),
+                      elevation: WidgetStatePropertyAll(10),
+                    ),
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.green,
+                      size: 50,
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(
+                        const Color.fromARGB(255, 219, 216, 216),
+                      ),
+                      elevation: WidgetStatePropertyAll(10),
+                    ),
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.restore_from_trash_rounded,
+                      color: Colors.green,
+                      size: 50,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
