26e0ad1f9dc4f6e6bc623b317cc63d9456504340
Eduardo Santana <santana.edu2004@gmail.com>
2025-05-13 14:06:18 -0300

    Arrumando compatibilidade com IOS, mas ainda falta testar

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
+import flutter_dotenv
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,14 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // Carrega as variáveis do .env
+    try? DotEnv().load()
+
+    // Obtém a chave da API do .env
+    let apiKey = DotEnv().get("GOOGLE_MAPS_API_KEY") ?? "CHAVE_NAO_ENCONTRADA"
+    GMSServices.provideAPIKey(apiKey)
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
